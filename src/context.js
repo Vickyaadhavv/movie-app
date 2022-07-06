93c87003aa3d3bdb4e36d02784a5b8d7a6d49caa
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-07-06 20:35:44 +0530

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 
 //const API_URL =`http://www.omdbapi.com/?i=tt3896198&apikey=24bdad83&s=titanic`
-export const API_URL =`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL =`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 const AppContext = React.createContext();
 
